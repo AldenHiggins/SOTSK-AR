4385442e1c40ff7ce2caadec4e66a27f943c0d64
DESKTOP-T9V311K\Alden Higgins <johnaldenhiggins@gmail.com>
2017-10-10 17:18:33 -0700

    Added a build script

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildScript : MonoBehaviour
+{
+    static void Build()
+    {
+        string[] scenes = { "Assets/GameFolder/Scenes/BaseScene.unity", "Assets/GameFolder/Scenes/RoomLightMap.unity", "Assets/GameFolder/Scenes/FieldLevel1.unity" };
+        BuildPipeline.BuildPlayer(scenes, "SOTSKBuild", BuildTarget.iOS, BuildOptions.None);
+    }
+}
